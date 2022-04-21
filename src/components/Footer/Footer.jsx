9c08d0def9535e4c13e79047aaadcffb43339f4e
DanielS4323 <daky4323@gmail.com>
2022-04-21 22:11:17 +0200

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+        <footer className="foot">
+                Powered By React Class
+        </footer>
+  )}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import logo from "../../assets/logo.png"
+import "./style.css"
+import { Link, NavLink } from 'react-router-dom'
+
+function Header() {
+  return (
+   <>
+   <nav className='navigation'>
+      <img className='logo' src={logo}/>
+      <ul className='pages'>
+        <li>
+          <NavLink className='links'
+           to='/'
+           style={({ isActive }) => ({
+            textDecoration: isActive ? 'underline' : ''
+            
+           })}
+          >Home
+          </NavLink>
+        </li>
+        <li>
+        <NavLink 
+        className='links'
+         style={({ isActive }) => ({
+             textDecoration: isActive ? 'underline' : ''
+            })} to='/about'>
+          About Us
+          </NavLink>
+        </li>
+        <li>
+        <NavLink className='links'
+         to='/contact'
+         style={({ isActive }) => ({
+          textDecoration: isActive ? 'underline' : ''
+         })}>
+          Contact
+          </NavLink>
+        </li>
+      </ul>
+   </nav>
+   </>
+  )
+}
+
+export default Header

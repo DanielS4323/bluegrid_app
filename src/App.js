89c728804189d5ba5bd2fef750dc79ce0514e8c1
DanielS4323 <daky4323@gmail.com>
2022-06-17 00:11:33 +0200

    context implement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -8,19 +7,13 @@ import Details from "./pages/Details";
 import ContactProvider from "./store/Contact-Context/ContactProvider";
 
 function App() {
-  const [articles, SetArticles] = useState([]);
-
   return (
     <ContactProvider>
       <Router>
         <Routes>
+          <Route element={<Home />} path="/" exact />
           <Route
-            element={<Home articles={articles} SetArticles={SetArticles} />}
-            path="/"
-            exact
-          />
-          <Route
-            element={<Details articles={articles} />}
+            element={<Details />}
             path="/details/:id/:articleTitle"
             exact
           />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './main.css'
+import "./main.css";
+import ArticleProvider from "./store/Article-Context/ArticleProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ArticleProvider>
+    <App />
+  </ArticleProvider>
+);
